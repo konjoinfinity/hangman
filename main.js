d7cf785914baf4b5cccc78581980d963c85191a9
Wesley Scholl <clients@konjoweb.com>
2019-02-05 17:34:39 -0500

    added interactivity upon each button letter click

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var hangmanWord = [];
+var spacesArray = [];
 
 const createWord = function() {
   hangmanWord.push(document.getElementById("hangmanword").value);
   console.log(hangmanWord);
+  document.getElementById("hangmanword").value = "";
   createSpaces();
 };
 
@@ -13,4 +15,137 @@ const createSpaces = function() {
     space.appendChild(letter);
     document.body.appendChild(space);
   }
+  console.log(document.querySelectorAll("div-space"));
+  spacesArray = hangmanword[0].length;
 };
+
+const letterA = document.getElementById("letterA");
+const letterB = document.getElementById("letterB");
+const letterC = document.getElementById("letterC");
+const letterD = document.getElementById("letterD");
+const letterE = document.getElementById("letterE");
+const letterF = document.getElementById("letterF");
+const letterG = document.getElementById("letterG");
+const letterH = document.getElementById("letterH");
+const letterI = document.getElementById("letterI");
+const letterJ = document.getElementById("letterJ");
+const letterK = document.getElementById("letterK");
+const letterL = document.getElementById("letterL");
+const letterM = document.getElementById("letterM");
+const letterN = document.getElementById("letterN");
+const letterO = document.getElementById("letterO");
+const letterP = document.getElementById("letterP");
+const letterQ = document.getElementById("letterQ");
+const letterR = document.getElementById("letterR");
+const letterS = document.getElementById("letterS");
+const letterT = document.getElementById("letterT");
+const letterU = document.getElementById("letterU");
+const letterV = document.getElementById("letterV");
+const letterW = document.getElementById("letterW");
+const letterX = document.getElementById("letterX");
+const letterY = document.getElementById("letterY");
+const letterZ = document.getElementById("letterZ");
+
+letterA.addEventListener("click", function(event) {
+  console.log(letterA.value);
+});
+
+letterB.addEventListener("click", function(event) {
+  console.log(letterB.value);
+});
+
+letterC.addEventListener("click", function(event) {
+  console.log(letterC.value);
+});
+
+letterD.addEventListener("click", function(event) {
+  console.log(letterD.value);
+});
+
+letterE.addEventListener("click", function(event) {
+  console.log(letterE.value);
+});
+
+letterF.addEventListener("click", function(event) {
+  console.log(letterF.value);
+});
+
+letterG.addEventListener("click", function(event) {
+  console.log(letterG.value);
+});
+
+letterH.addEventListener("click", function(event) {
+  console.log(letterH.value);
+});
+
+letterI.addEventListener("click", function(event) {
+  console.log(letterI.value);
+});
+
+letterJ.addEventListener("click", function(event) {
+  console.log(letterJ.value);
+});
+
+letterK.addEventListener("click", function(event) {
+  console.log(letterK.value);
+});
+
+letterL.addEventListener("click", function(event) {
+  console.log(letterL.value);
+});
+
+letterM.addEventListener("click", function(event) {
+  console.log(letterM.value);
+});
+
+letterN.addEventListener("click", function(event) {
+  console.log(letterN.value);
+});
+
+letterO.addEventListener("click", function(event) {
+  console.log(letterO.value);
+});
+
+letterP.addEventListener("click", function(event) {
+  console.log(letterP.value);
+});
+
+letterQ.addEventListener("click", function(event) {
+  console.log(letterQ.value);
+});
+
+letterR.addEventListener("click", function(event) {
+  console.log(letterR.value);
+});
+
+letterS.addEventListener("click", function(event) {
+  console.log(letterS.value);
+});
+
+letterT.addEventListener("click", function(event) {
+  console.log(letterT.value);
+});
+
+letterU.addEventListener("click", function(event) {
+  console.log(letterU.value);
+});
+
+letterV.addEventListener("click", function(event) {
+  console.log(letterV.value);
+});
+
+letterW.addEventListener("click", function(event) {
+  console.log(letterW.value);
+});
+
+letterX.addEventListener("click", function(event) {
+  console.log(letterX.value);
+});
+
+letterY.addEventListener("click", function(event) {
+  console.log(letterY.value);
+});
+
+letterZ.addEventListener("click", function(event) {
+  console.log(letterZ.value);
+});
