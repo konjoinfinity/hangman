d10778e2976a8ed25fe5bbcabea7ecee865dc729
Wesley Scholl <clients@konjoweb.com>
2019-02-05 16:03:35 -0500

    added functionality to add number of spaces based on hangmanWord[0].length

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-console.log("konjo!");
-
 var hangmanWord = [];
 
 const createWord = function() {
   hangmanWord.push(document.getElementById("hangmanword").value);
   console.log(hangmanWord);
+  createSpaces();
+};
+
+const createSpaces = function() {
+  for (i = 0; i < hangmanWord[0].length; i++) {
+    var space = document.createElement("P");
+    var letter = document.createTextNode("_ ");
+    space.appendChild(letter);
+    document.body.appendChild(space);
+  }
 };
