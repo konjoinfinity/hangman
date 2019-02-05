1cb53c8fbbe88bc890e97a0b7102a19e82dcc79a
Wesley Scholl <clients@konjoweb.com>
2019-02-05 15:23:30 -0500

    added hangman word input field and submit button

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("konjo!");
+
+const hangmanWord = [];
+
+const submitButton = document.querySelector(".submit");
+
+submitButton.addEventListener("click", function(evt) {
+  hangmanWord.push(document.querySelector(".text"));
+  console.log("konjo");
+  event.preventDefault(submitButton);
+});
