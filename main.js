19c6d93102b2324aa6838d962db24c733cb5b40a
Wesley Scholl <clients@konjoweb.com>
2019-02-06 16:50:06 -0500

    fixed win check function commented out extra letter button console.logs

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var hangmanWord = [];
 var spacesArray = [];
 var hangmanString = "";
-var hiddenLetter = [];
 
 const createWord = function() {
   hangmanString = document.getElementById("hangmanword").value;
@@ -15,14 +14,12 @@ const createWord = function() {
 
 const createSpaces = function() {
   for (i = 0; i < hangmanWord[0].length; i++) {
-    var para = document.createElement("p");
+    var para = document.createElement(`p${i}`);
     var t = document.createTextNode("_ ");
     para.appendChild(t);
     var parent = document.getElementById("wordboard");
     document.getElementById("wordboard").appendChild(para);
-    hiddenLetter.push(hangmanString.charAt(i));
   }
-  console.log(hiddenLetter);
   spacesArray = hangmanWord[0].length;
   //console.log(hangmanWord[0].length);
   document.getElementById("hangmanword").value = "";
@@ -31,14 +28,14 @@ const createSpaces = function() {
 var lettersCorrect = [];
 
 const checkLetter = function(letter) {
+  //console.log(letter);
   for (i = 0; i < hangmanWord[0].length; i++) {
     if (hangmanString.charAt(i) == `${letter}`) {
-      console.log(`${hiddenLetter[i]}`);
-      lettersCorrect = lettersCorrect + `${letter}`;
-      var blankSpace = document.querySelector("p"[i]);
+      //console.log(`${i}`);
+      var blankSpace = document.querySelector(`p${i}`);
       var parent = blankSpace.parentNode;
       var chosenLetter = hangmanString.charAt(i);
-      var letterSpace = document.createElement("p"[i]);
+      var letterSpace = document.createElement(`p${i}`);
       letterSpace.innerHTML = `${chosenLetter}`;
       parent.replaceChild(letterSpace, blankSpace);
     }
@@ -47,14 +44,14 @@ const checkLetter = function(letter) {
 };
 
 const checkWord = function() {
-  const divQuery = document
-    .querySelector("#wordboard")
-    .innerText.replace(/(\r\n|\n|\r)/gm, "");
-  console.log(divQuery);
-  if (divQuery.includes("_") == true) {
-    console.log("Keep Guessing...");
-  } else {
+  if (
+    document
+      .querySelector("#wordboard")
+      .innerText.replace(/(\r\n|\n|\r)/gm, "") == hangmanString
+  ) {
     console.log("You Win!");
+  } else {
+    console.log("Keep Guessing...");
   }
 };
 
@@ -86,157 +83,157 @@ const letterY = document.getElementById("letterY");
 const letterZ = document.getElementById("letterZ");
 
 letterA.addEventListener("click", function(event) {
-  console.log(letterA.value);
+  //console.log(letterA.value);
   checkLetter(`${letterA.value}`);
   document.getElementById("letterA").disabled = true;
 });
 
 letterB.addEventListener("click", function(event) {
-  console.log(letterB.value);
+  //console.log(letterB.value);
   checkLetter(`${letterB.value}`);
   document.getElementById("letterB").disabled = true;
 });
 
 letterC.addEventListener("click", function(event) {
-  console.log(letterC.value);
+  //console.log(letterC.value);
   checkLetter(`${letterC.value}`);
   document.getElementById("letterC").disabled = true;
 });
 
 letterD.addEventListener("click", function(event) {
-  console.log(letterD.value);
+  //console.log(letterD.value);
   checkLetter(`${letterD.value}`);
   document.getElementById("letterD").disabled = true;
 });
 
 letterE.addEventListener("click", function(event) {
-  console.log(letterE.value);
+  //console.log(letterE.value);
   checkLetter(`${letterE.value}`);
   document.getElementById("letterE").disabled = true;
 });
 
 letterF.addEventListener("click", function(event) {
-  console.log(letterF.value);
+  //console.log(letterF.value);
   checkLetter(`${letterF.value}`);
   document.getElementById("letterF").disabled = true;
 });
 
 letterG.addEventListener("click", function(event) {
-  console.log(letterG.value);
+  //console.log(letterG.value);
   checkLetter(`${letterG.value}`);
   document.getElementById("letterG").disabled = true;
 });
 
 letterH.addEventListener("click", function(event) {
-  console.log(letterH.value);
+  //console.log(letterH.value);
   checkLetter(`${letterH.value}`);
   document.getElementById("letterH").disabled = true;
 });
 
 letterI.addEventListener("click", function(event) {
-  console.log(letterI.value);
+  //console.log(letterI.value);
   checkLetter(`${letterI.value}`);
   document.getElementById("letterI").disabled = true;
 });
 
 letterJ.addEventListener("click", function(event) {
-  console.log(letterJ.value);
+  //console.log(letterJ.value);
   checkLetter(`${letterJ.value}`);
   document.getElementById("letterJ").disabled = true;
 });
 
 letterK.addEventListener("click", function(event) {
-  console.log(letterK.value);
+  //console.log(letterK.value);
   checkLetter(`${letterK.value}`);
   document.getElementById("letterK").disabled = true;
 });
 
 letterL.addEventListener("click", function(event) {
-  console.log(letterL.value);
+  //console.log(letterL.value);
   checkLetter(`${letterL.value}`);
   document.getElementById("letterL").disabled = true;
 });
 
 letterM.addEventListener("click", function(event) {
-  console.log(letterM.value);
+  //console.log(letterM.value);
   checkLetter(`${letterM.value}`);
   document.getElementById("letterM").disabled = true;
 });
 
 letterN.addEventListener("click", function(event) {
-  console.log(letterN.value);
+  //console.log(letterN.value);
   checkLetter(`${letterN.value}`);
   document.getElementById("letterN").disabled = true;
 });
 
 letterO.addEventListener("click", function(event) {
-  console.log(letterO.value);
+  //console.log(letterO.value);
   checkLetter(`${letterO.value}`);
   document.getElementById("letterO").disabled = true;
 });
 
 letterP.addEventListener("click", function(event) {
-  console.log(letterP.value);
+  //console.log(letterP.value);
   checkLetter(`${letterP.value}`);
   document.getElementById("letterP").disabled = true;
 });
 
 letterQ.addEventListener("click", function(event) {
-  console.log(letterQ.value);
+  //console.log(letterQ.value);
   checkLetter(`${letterQ.value}`);
   document.getElementById("letterQ").disabled = true;
 });
 
 letterR.addEventListener("click", function(event) {
-  console.log(letterR.value);
+  //console.log(letterR.value);
   checkLetter(`${letterR.value}`);
   document.getElementById("letterR").disabled = true;
 });
 
 letterS.addEventListener("click", function(event) {
-  console.log(letterS.value);
+  //console.log(letterS.value);
   checkLetter(`${letterS.value}`);
   document.getElementById("letterS").disabled = true;
 });
 
 letterT.addEventListener("click", function(event) {
-  console.log(letterT.value);
+  //console.log(letterT.value);
   checkLetter(`${letterT.value}`);
   document.getElementById("letterT").disabled = true;
 });
 
 letterU.addEventListener("click", function(event) {
-  console.log(letterU.value);
+  //console.log(letterU.value);
   checkLetter(`${letterU.value}`);
   document.getElementById("letterU").disabled = true;
 });
 
 letterV.addEventListener("click", function(event) {
-  console.log(letterV.value);
+  //console.log(letterV.value);
   checkLetter(`${letterV.value}`);
   document.getElementById("letterV").disabled = true;
 });
 
 letterW.addEventListener("click", function(event) {
-  console.log(letterW.value);
+  //console.log(letterW.value);
   checkLetter(`${letterW.value}`);
   document.getElementById("letterW").disabled = true;
 });
 
 letterX.addEventListener("click", function(event) {
-  console.log(letterX.value);
+  //console.log(letterX.value);
   checkLetter(`${letterX.value}`);
   document.getElementById("letterX").disabled = true;
 });
 
 letterY.addEventListener("click", function(event) {
-  console.log(letterY.value);
+  //console.log(letterY.value);
   checkLetter(`${letterY.value}`);
   document.getElementById("letterY").disabled = true;
 });
 
 letterZ.addEventListener("click", function(event) {
-  console.log(letterZ.value);
+  //console.log(letterZ.value);
   checkLetter(`${letterZ.value}`);
   document.getElementById("letterZ").disabled = true;
 });
