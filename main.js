211a29b60b1bdc81d7281eda32395841ca5ca40b
Wesley Scholl <clients@konjoweb.com>
2019-02-06 09:41:21 -0500

    added checkLetter for all letters, added uppercase to prevent string mismatches

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const createWord = function() {
   console.log(hangmanString);
   hangmanWord.push(document.getElementById("hangmanword").value);
   console.log(hangmanWord);
+  hangmanString = hangmanString.toUpperCase();
   createSpaces();
 };
 
@@ -63,105 +64,130 @@ const letterZ = document.getElementById("letterZ");
 
 letterA.addEventListener("click", function(event) {
   console.log(letterA.value);
-  checkLetter("a");
+  checkLetter(`${letterA.value}`);
 });
 
 letterB.addEventListener("click", function(event) {
   console.log(letterB.value);
+  checkLetter(`${letterB.value}`);
 });
 
 letterC.addEventListener("click", function(event) {
   console.log(letterC.value);
+  checkLetter(`${letterC.value}`);
 });
 
 letterD.addEventListener("click", function(event) {
   console.log(letterD.value);
+  checkLetter(`${letterD.value}`);
 });
 
 letterE.addEventListener("click", function(event) {
   console.log(letterE.value);
+  checkLetter(`${letterE.value}`);
 });
 
 letterF.addEventListener("click", function(event) {
   console.log(letterF.value);
+  checkLetter(`${letterF.value}`);
 });
 
 letterG.addEventListener("click", function(event) {
   console.log(letterG.value);
+  checkLetter(`${letterG.value}`);
 });
 
 letterH.addEventListener("click", function(event) {
   console.log(letterH.value);
+  checkLetter(`${letterH.value}`);
 });
 
 letterI.addEventListener("click", function(event) {
   console.log(letterI.value);
+  checkLetter(`${letterI.value}`);
 });
 
 letterJ.addEventListener("click", function(event) {
   console.log(letterJ.value);
+  checkLetter(`${letterJ.value}`);
 });
 
 letterK.addEventListener("click", function(event) {
   console.log(letterK.value);
+  checkLetter(`${letterK.value}`);
 });
 
 letterL.addEventListener("click", function(event) {
   console.log(letterL.value);
+  checkLetter(`${letterL.value}`);
 });
 
 letterM.addEventListener("click", function(event) {
   console.log(letterM.value);
+  checkLetter(`${letterM.value}`);
 });
 
 letterN.addEventListener("click", function(event) {
   console.log(letterN.value);
+  checkLetter(`${letterN.value}`);
 });
 
 letterO.addEventListener("click", function(event) {
   console.log(letterO.value);
+  checkLetter(`${letterO.value}`);
 });
 
 letterP.addEventListener("click", function(event) {
   console.log(letterP.value);
+  checkLetter(`${letterP.value}`);
 });
 
 letterQ.addEventListener("click", function(event) {
   console.log(letterQ.value);
+  checkLetter(`${letterQ.value}`);
 });
 
 letterR.addEventListener("click", function(event) {
   console.log(letterR.value);
+  checkLetter(`${letterR.value}`);
 });
 
 letterS.addEventListener("click", function(event) {
   console.log(letterS.value);
+  checkLetter(`${letterS.value}`);
 });
 
 letterT.addEventListener("click", function(event) {
   console.log(letterT.value);
+  checkLetter(`${letterT.value}`);
 });
 
 letterU.addEventListener("click", function(event) {
   console.log(letterU.value);
+  checkLetter(`${letterU.value}`);
 });
 
 letterV.addEventListener("click", function(event) {
   console.log(letterV.value);
+  checkLetter(`${letterV.value}`);
 });
 
 letterW.addEventListener("click", function(event) {
   console.log(letterW.value);
+  checkLetter(`${letterW.value}`);
 });
 
 letterX.addEventListener("click", function(event) {
   console.log(letterX.value);
+  checkLetter(`${letterX.value}`);
 });
 
 letterY.addEventListener("click", function(event) {
   console.log(letterY.value);
+  checkLetter(`${letterY.value}`);
 });
 
 letterZ.addEventListener("click", function(event) {
   console.log(letterZ.value);
+  checkLetter(`${letterZ.value}`);
 });
