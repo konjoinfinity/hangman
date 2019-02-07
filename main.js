7cd49b545fb8ab31f72f897318410cbb8af69e74
Wesley Scholl <clients@konjoweb.com>
2019-02-07 10:21:43 -0500

    removed duplicate checkLetter function, called twice from html and js

--- main.js
+++ main.js
@@ -96,156 +96,130 @@ const letterZ = document.getElementById("letterZ");
 
 letterA.addEventListener("click", function(event) {
   //console.log(letterA.value);
-
   document.getElementById("letterA").disabled = true;
 });
 
 letterB.addEventListener("click", function(event) {
   //console.log(letterB.value);
-  checkLetter(`${letterB.value}`);
   document.getElementById("letterB").disabled = true;
 });
 
 letterC.addEventListener("click", function(event) {
   //console.log(letterC.value);
-  checkLetter(`${letterC.value}`);
   document.getElementById("letterC").disabled = true;
 });
 
 letterD.addEventListener("click", function(event) {
   //console.log(letterD.value);
-  checkLetter(`${letterD.value}`);
   document.getElementById("letterD").disabled = true;
 });
 
 letterE.addEventListener("click", function(event) {
   //console.log(letterE.value);
-  checkLetter(`${letterE.value}`);
   document.getElementById("letterE").disabled = true;
 });
 
 letterF.addEventListener("click", function(event) {
   //console.log(letterF.value);
-  checkLetter(`${letterF.value}`);
   document.getElementById("letterF").disabled = true;
 });
 
 letterG.addEventListener("click", function(event) {
   //console.log(letterG.value);
-  checkLetter(`${letterG.value}`);
   document.getElementById("letterG").disabled = true;
 });
 
 letterH.addEventListener("click", function(event) {
   //console.log(letterH.value);
-  checkLetter(`${letterH.value}`);
   document.getElementById("letterH").disabled = true;
 });
 
 letterI.addEventListener("click", function(event) {
   //console.log(letterI.value);
-  checkLetter(`${letterI.value}`);
   document.getElementById("letterI").disabled = true;
 });
 
 letterJ.addEventListener("click", function(event) {
   //console.log(letterJ.value);
-  checkLetter(`${letterJ.value}`);
   document.getElementById("letterJ").disabled = true;
 });
 
 letterK.addEventListener("click", function(event) {
   //console.log(letterK.value);
-  checkLetter(`${letterK.value}`);
   document.getElementById("letterK").disabled = true;
 });
 
 letterL.addEventListener("click", function(event) {
   //console.log(letterL.value);
-  checkLetter(`${letterL.value}`);
   document.getElementById("letterL").disabled = true;
 });
 
 letterM.addEventListener("click", function(event) {
   //console.log(letterM.value);
-  checkLetter(`${letterM.value}`);
   document.getElementById("letterM").disabled = true;
 });
 
 letterN.addEventListener("click", function(event) {
   //console.log(letterN.value);
-  checkLetter(`${letterN.value}`);
   document.getElementById("letterN").disabled = true;
 });
 
 letterO.addEventListener("click", function(event) {
   //console.log(letterO.value);
-  checkLetter(`${letterO.value}`);
   document.getElementById("letterO").disabled = true;
 });
 
 letterP.addEventListener("click", function(event) {
   //console.log(letterP.value);
-  checkLetter(`${letterP.value}`);
   document.getElementById("letterP").disabled = true;
 });
 
 letterQ.addEventListener("click", function(event) {
   //console.log(letterQ.value);
-  checkLetter(`${letterQ.value}`);
   document.getElementById("letterQ").disabled = true;
 });
 
 letterR.addEventListener("click", function(event) {
   //console.log(letterR.value);
-  checkLetter(`${letterR.value}`);
   document.getElementById("letterR").disabled = true;
 });
 
 letterS.addEventListener("click", function(event) {
   //console.log(letterS.value);
-  checkLetter(`${letterS.value}`);
   document.getElementById("letterS").disabled = true;
 });
 
 letterT.addEventListener("click", function(event) {
   //console.log(letterT.value);
-  checkLetter(`${letterT.value}`);
   document.getElementById("letterT").disabled = true;
 });
 
 letterU.addEventListener("click", function(event) {
   //console.log(letterU.value);
-  checkLetter(`${letterU.value}`);
   document.getElementById("letterU").disabled = true;
 });
 
 letterV.addEventListener("click", function(event) {
   //console.log(letterV.value);
-  checkLetter(`${letterV.value}`);
   document.getElementById("letterV").disabled = true;
 });
 
 letterW.addEventListener("click", function(event) {
   //console.log(letterW.value);
-  checkLetter(`${letterW.value}`);
   document.getElementById("letterW").disabled = true;
 });
 
 letterX.addEventListener("click", function(event) {
   //console.log(letterX.value);
-  checkLetter(`${letterX.value}`);
   document.getElementById("letterX").disabled = true;
 });
 
 letterY.addEventListener("click", function(event) {
   //console.log(letterY.value);
-  checkLetter(`${letterY.value}`);
   document.getElementById("letterY").disabled = true;
 });
 
 letterZ.addEventListener("click", function(event) {
   //console.log(letterZ.value);
-  checkLetter(`${letterZ.value}`);
   document.getElementById("letterZ").disabled = true;
 });
